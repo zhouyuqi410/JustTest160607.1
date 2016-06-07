92dbccfb9a29493b9b38e2080dbcf1fa010fb23b
zhouyuqi410 <zhouyuqi410@163.com>
2016-06-07 17:46:00 +0800

    1

---
+++ JustTest2/JustTest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JustTest2
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 周玉琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JustTest2/JustTest2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JustTest2
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 周玉琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
